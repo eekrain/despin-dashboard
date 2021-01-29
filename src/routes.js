1163c482b2d4752564f5c7540c83cb893f5a5fdd
Ardian Eka Candra <ardianoption10@gmail.com>
2021-01-30 02:25:09 +0700

    init template

---
+++ src/routes.js
@@ -0,0 +1,140 @@
+import React from "react";
+import $ from "jquery";
+
+window.jQuery = $;
+window.$ = $;
+global.jQuery = $;
+
+const DashboardDefault = React.lazy(() =>
+  import("./datta-able/Demo/Dashboard/Default")
+);
+
+const UIBasicButton = React.lazy(() =>
+  import("./datta-able/Demo/UIElements/Basic/Button")
+);
+const UIBasicBadges = React.lazy(() =>
+  import("./datta-able/Demo/UIElements/Basic/Badges")
+);
+const UIBasicBreadcrumbPagination = React.lazy(() =>
+  import("./datta-able/Demo/UIElements/Basic/BreadcrumbPagination")
+);
+
+const UIBasicCollapse = React.lazy(() =>
+  import("./datta-able/Demo/UIElements/Basic/Collapse")
+);
+const UIBasicTabsPills = React.lazy(() =>
+  import("./datta-able/Demo/UIElements/Basic/TabsPills")
+);
+const UIBasicBasicTypography = React.lazy(() =>
+  import("./datta-able/Demo/UIElements/Basic/Typography")
+);
+
+const FormsElements = React.lazy(() =>
+  import("./datta-able/Demo/Forms/FormsElements")
+);
+
+const BootstrapTable = React.lazy(() =>
+  import("./datta-able/Demo/Tables/BootstrapTable")
+);
+
+const Nvd3Chart = React.lazy(() =>
+  import("./datta-able/Demo/Charts/Nvd3Chart/index")
+);
+
+const GoogleMap = React.lazy(() =>
+  import("./datta-able/Demo/Maps/GoogleMap/index")
+);
+
+const OtherSamplePage = React.lazy(() =>
+  import("./datta-able/Demo/Other/SamplePage")
+);
+const OtherDocs = React.lazy(() => import("./datta-able/Demo/Other/Docs"));
+
+// ====================================================================================================================
+
+const ArtikelCreate = React.lazy(() => import("./pages/artikel/create"));
+
+// ====================================================================================================================
+const routes = [
+  {
+    path: "/artikel/create",
+    exact: true,
+    name: "Default",
+    component: ArtikelCreate,
+  },
+  {
+    path: "/dashboard/default",
+    exact: true,
+    name: "Default",
+    component: DashboardDefault,
+  },
+  {
+    path: "/basic/button",
+    exact: true,
+    name: "Basic Button",
+    component: UIBasicButton,
+  },
+  {
+    path: "/basic/badges",
+    exact: true,
+    name: "Basic Badges",
+    component: UIBasicBadges,
+  },
+  {
+    path: "/basic/breadcrumb-paging",
+    exact: true,
+    name: "Basic Breadcrumb Pagination",
+    component: UIBasicBreadcrumbPagination,
+  },
+  {
+    path: "/basic/collapse",
+    exact: true,
+    name: "Basic Collapse",
+    component: UIBasicCollapse,
+  },
+  {
+    path: "/basic/tabs-pills",
+    exact: true,
+    name: "Basic Tabs & Pills",
+    component: UIBasicTabsPills,
+  },
+  {
+    path: "/basic/typography",
+    exact: true,
+    name: "Basic Typography",
+    component: UIBasicBasicTypography,
+  },
+  {
+    path: "/forms/form-basic",
+    exact: true,
+    name: "Forms Elements",
+    component: FormsElements,
+  },
+  {
+    path: "/tables/bootstrap",
+    exact: true,
+    name: "Bootstrap Table",
+    component: BootstrapTable,
+  },
+  {
+    path: "/charts/nvd3",
+    exact: true,
+    name: "Nvd3 Chart",
+    component: Nvd3Chart,
+  },
+  {
+    path: "/maps/google-map",
+    exact: true,
+    name: "Google Map",
+    component: GoogleMap,
+  },
+  {
+    path: "/sample-page",
+    exact: true,
+    name: "Sample Page",
+    component: OtherSamplePage,
+  },
+  { path: "/docs", exact: true, name: "Documentation", component: OtherDocs },
+];
+
+export default routes;
